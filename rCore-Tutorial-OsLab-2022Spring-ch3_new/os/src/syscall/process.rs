9e292e87e809d6ea79bc936d7e9a9fc9f9f46e87
Venie <117576967+xx071@users.noreply.github.com>
2023-12-22 19:43:29 +0800

    Add files via upload

---
+++ rCore-Tutorial-OsLab-2022Spring-ch3_new/os/src/syscall/process.rs
@@ -0,0 +1,36 @@
+use crate::task::{exit_current_and_run_next, suspend_current_and_run_next, TaskInfo, TaskStatus};
+use crate::timer::get_time_us;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
+}
+
+pub fn sys_exit(exit_code: i32) -> ! {
+    info!("[kernel] Application exited with code {}", exit_code);
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+pub fn sys_yield() -> isize {
+    suspend_current_and_run_next();
+    0
+}
+
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    let us = get_time_us();
+    unsafe {
+        *ts = TimeVal {
+            sec: us / 1_000_000,
+            usec: us % 1_000_000,
+        };
+    }
+    0
+}
+
+// YOUR JOB: Finish sys_task_info to pass testcases
+pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
+    -1
+}
